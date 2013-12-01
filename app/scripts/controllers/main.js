ffef6b5dcc9fcd85b52eeb2ac566d5432859f9e5
David Romaní <david@flux.cat>
2013-12-01 16:47:12 +0100

    step 4.9 - avoid async data load

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,21 +17,21 @@ riureLoPatiControllers.controller('MainCtrl', ['$scope', '$http', '$location', '
 
 riureLoPatiControllers.controller('PageDetailCtrl', ['$scope', '$http', '$routeParams', '$interval', '$sce', 'Layout',
     function($scope, $http, $routeParams, $interval, $sce, Layout) {
-        $http.get('data/pages.json').success(function(data) {
-            $scope.pages = data;
-            var index = 0;
-            var found = false;
-            while (!found && index < $scope.pages.length) {
-              if ($scope.pages[index].id === parseInt($routeParams.pageId, 10)) {
-                found = true;
-                $scope.currentPage = $scope.pages[index];
-                Layout.setTitle($scope.currentPage.metatile);
-                Layout.setDescription($scope.currentPage.metadescription);
-                $scope.textPanel = $scope.currentPage.text.substring(0, $scope.currentPage.text.length - $scope.textPanelPointer);
-              }
-              index++;
-            }
-          });
+        /* Avoid async load for SEO reasons */
+        /*$http.get('data/pages.json').success(function(data) {
+          });*/
+        $scope.pages = pages;
+        var index = 0;
+        var found = false;
+        while (!found && index < $scope.pages.length) {
+          if ($scope.pages[index].id === parseInt($routeParams.pageId, 10)) {
+            found = true;
+            $scope.currentPage = $scope.pages[index];
+            Layout.setTitle($scope.currentPage.metatile);
+            Layout.setDescription($scope.currentPage.metadescription);
+          }
+          index++;
+        }
         $scope.textPanelCollapsed = true;
         $scope.trustSrc = function(src) {
             return $sce.trustAsResourceUrl(src);
