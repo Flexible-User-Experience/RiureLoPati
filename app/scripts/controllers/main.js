2e44cec78f2b0885bbf888709b11196d7284698e
David Romaní <david@flux.cat>
2015-01-19 10:34:49 +0100

    clean jshint results 3

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,57 +6,57 @@
 var riureLoPatiControllers = angular.module('riureLoPatiControllers', []);
 
 riureLoPatiControllers.controller('MainCtrl', ['$scope', '$http', '$location', 'Layout',
-    function($scope, $http, $location, Layout) {
+    function ($scope, $http, $location, Layout) {
         $location.path('/2/ca');
-      }]);
+    }]);
 
 riureLoPatiControllers.controller('DefaultPageDetailCtrl', ['$scope', '$http', '$location', 'Layout',
-    function($scope, $http, $location, Layout) {
+    function ($scope, $http, $location, Layout) {
         $location.path('/2/ca');
-      }]);
+    }]);
 
 riureLoPatiControllers.controller('PageDetailCtrl', ['$scope', '$http', '$routeParams', '$interval', '$sce', 'Layout',
-    function($scope, $http, $routeParams, $interval, $sce, Layout) {
+    function ($scope, $http, $routeParams, $interval, $sce, Layout) {
         /* Avoid async load for SEO reasons */
         /*$http.get('data/pages.json').success(function(data) {
-          });*/
+         });*/
         if ($routeParams.locale === 'es') {
-          $scope.pages = pageses;
+            $scope.pages = pageses;
         } else {
-          $scope.pages = pagesca;
+            $scope.pages = pagesca;
         }
         var index = 0;
         var found = false;
         while (!found && index < $scope.pages.length) {
-          if ($scope.pages[index].id === parseInt($routeParams.pageId, 10)) {
-            found = true;
-            $scope.currentPage = $scope.pages[index];
-            Layout.setId($scope.currentPage.id);
-            Layout.setTitle($scope.currentPage.metatitle);
-            Layout.setDescription($scope.currentPage.metadescription);
-          }
-          index++;
+            if ($scope.pages[index].id === parseInt($routeParams.pageId, 10)) {
+                found = true;
+                $scope.currentPage = $scope.pages[index];
+                Layout.setId($scope.currentPage.id);
+                Layout.setTitle($scope.currentPage.metatitle);
+                Layout.setDescription($scope.currentPage.metadescription);
+            }
+            index++;
         }
         $scope.textPanelCollapsed = true;
-        $scope.trustSrc = function(src) {
+        $scope.trustSrc = function (src) {
             return $sce.trustAsResourceUrl(src);
-          };
-        $scope.getImagePath = function(path) {
+        };
+        $scope.getImagePath = function (path) {
             return 'images/' + path;
-          };
-        $scope.getThumbImagePath = function(path) {
+        };
+        $scope.getThumbImagePath = function (path) {
             return 'images/thumbs/' + path;
-          };
-        $scope.getAttatchmentPath = function(path) {
+        };
+        $scope.getAttatchmentPath = function (path) {
             return 'attatchments/' + path;
-          };
-      }]);
+        };
+    }]);
 
 riureLoPatiControllers.controller('LayoutCtrl', ['$scope', '$http', 'Layout',
-    function($scope, $http, Layout) {
+    function ($scope, $http, Layout) {
         $scope.Layout = Layout;
-      }]);
+    }]);
 
 function moveMe() {
-  jQuery('html,body').animate({scrollLeft: jQuery("#tope").offset().left-1000}, 5500, 'easeOutQuad');
+    jQuery('html,body').animate({scrollLeft: jQuery("#tope").offset().left - 1000}, 5500, 'easeOutQuad');
 }
